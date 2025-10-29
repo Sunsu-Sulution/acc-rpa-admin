3c42a1d8234060a85ea3b50c3472dad13f566882
algnot <thanawat.tongla@gmail.com>
2025-10-29 10:13:28 +0700

    Improve shop dashboard by adding 'Line EDC' column to DataTable and enhancing activity logging for user actions. Update user feedback in Activity Log component for better clarity.

---
+++ app/dashboard/logs/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>You don&apos;t permission to see this content</div>;
+}
---
+++ app/dashboard/user-history/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>You don&apos;t permission to see this content</div>
+  )
+}
