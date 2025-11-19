d367f092f209d5103121d89371dab6d05c0f9c43
algnot <thanawat.tongla@gmail.com>
2025-11-19 15:03:04 +0700

    Update GET route to convert 'amout' parameter from string to number for proper processing

--- app/api/convert/[amout]/route.ts
+++ app/api/convert/[amout]/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { convert } from 'baht';
 
-export async function GET(request: NextRequest, { params }: { params: { amout: number } }) {
+export async function GET(request: NextRequest, { params }: { params: { amout: string } }) {
     try {
-        const amoutNumber = params.amout;
+        const amoutNumber = Number(params.amout);
         return NextResponse.json({
             "result": convert(amoutNumber)
         });
