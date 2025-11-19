b9d1245672d1ab078399e202d2aab21bbdcdb07a
algnot <thanawat.tongla@gmail.com>
2025-11-19 15:04:56 +0700

    Refactor GET route to await 'amout' parameter in context for improved async handling

--- app/api/convert/[amout]/route.ts
+++ app/api/convert/[amout]/route.ts
@@ -1,8 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { convert } from 'baht';
 
-export async function GET(request: NextRequest, { params }: { params: { amout: string } }) {
+export async function GET(
+    request: NextRequest,
+    context: { params: Promise<{ amout: string }> }
+) {
     try {
+        const params = await context.params;
         const amoutNumber = Number(params.amout);
         return NextResponse.json({
             "result": convert(amoutNumber)
